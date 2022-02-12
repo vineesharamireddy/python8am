2a83f23fb188b5941213b43b0e37f4ff244934ea
vineesharamireddy <37893114+vineesharamireddy@users.noreply.github.com>
2022-02-12 22:58:40 +0530

    Add files via upload

--- 7-feb/list.py
+++ 7-feb/list.py
@@ -1,10 +1,10 @@
 #List
 #Create an empty list (two ways)
 
-li1=[]
-print(li1)
-li2=list()
-print(li2)
+li=[]
+print(li)
+l=list()
+print(l)
 
 output:
 
@@ -13,10 +13,11 @@ output:
 
 #Concatenate with [5,6,7,8]
 
-li3=[5,6,7,8]
-li4=[8,9,1,5,6,7,8]
+li1=[5,6,7,8]
+li2=li+li1
+li3=[8,9,1,5,6,7,8]
+print(li2)
 print(li3)
-print(li4)
 
 
 output:
@@ -41,6 +42,13 @@ output:
 2
 
 #find the mean of the list
+
+print("mean of li1 is",(sum(li1))/len(li1))
+
+output:
+mean of li1 is 5.5
+
+
 #find sum (List) + min + Max 
 
 print(min(li4))
@@ -59,6 +67,21 @@ output:
 
 #Find median of the list
 
+li1.sort()
+a=int((len(li1)/2))
+print(a)
+print((li1[a]+li1[a+1])/2)
+
+output:
+
+[9, 1, 4, 8, 8]
+2
+8.0
+
 #remove duplicates from list and give output in the format of tuple
 
+print(set(tuple(li1)))
 
+output:
+
+{8, 1, 4, 9}
